3e4b815fe5870749f75487da011998c7a2d62da0
ALMS Import <noreply@example.com>
2026-04-08 19:36:16 +0600

    feat(sidebar): restore profile-pic upload form and make avatar clickable for employees

--- resources/views/partials/sidebar_profile.blade.php
+++ resources/views/partials/sidebar_profile.blade.php
@@ -13,11 +13,26 @@
 @endphp
 
 <div class="profile">
+    @php $showUpload = !($isAdminArea ?? false) && auth()->check(); @endphp
     <div class="avatar">
-        @if(auth()->check() && auth()->user()->profile_picture && file_exists(public_path('profile_pictures/' . auth()->user()->profile_picture)))
-            <img src="{{ asset('profile_pictures/' . auth()->user()->profile_picture) }}" alt="Profile">
+        @if($showUpload)
+            <form id="profilePicForm" method="POST" action="{{ route('dashboard.profile.picture') }}" enctype="multipart/form-data" style="display:inline;">
+                @csrf
+                <label for="profilePicInput" style="cursor:pointer;display:block;">
+                    @if(auth()->user()->profile_picture && file_exists(public_path('profile_pictures/' . auth()->user()->profile_picture)))
+                        <img src="{{ asset('profile_pictures/' . auth()->user()->profile_picture) }}" alt="Profile">
+                    @else
+                        {{ $initials }}
+                    @endif
+                </label>
+                <input id="profilePicInput" name="profile_picture" type="file" accept="image/*" style="display:none">
+            </form>
         @else
-            {{ $initials }}
+            @if(auth()->check() && auth()->user()->profile_picture && file_exists(public_path('profile_pictures/' . auth()->user()->profile_picture)))
+                <img src="{{ asset('profile_pictures/' . auth()->user()->profile_picture) }}" alt="Profile">
+            @else
+                {{ $initials }}
+            @endif
         @endif
     </div>
     <div class="username">{{ $sidebarName }}</div>
