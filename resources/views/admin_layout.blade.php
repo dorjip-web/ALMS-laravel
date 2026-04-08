c9f870b8e57983d3fa0cab1b63d57e487435c942
ALMS Import <noreply@example.com>
2026-04-08 12:04:11 +0600

    Remove Settings submenu from sidebar

--- resources/views/admin_layout.blade.php
+++ resources/views/admin_layout.blade.php
@@ -33,15 +33,7 @@
             <a href="{{ route('admin.attendance_logs.index') }}" @class(['active' => ($activeNav ?? '') === 'attendance_logs'])>Attendance Logs</a>
             <a href="{{ route('admin.leave_records.index') }}" @class(['active' => ($activeNav ?? '') === 'leave_records'])>Leave Records</a>
             {{-- Reports link removed per request --}}
-            <div class="menu-item">
-                    <a href="{{ route('admin.settings.index') }}" @class(['active' => ($activeNav ?? '') === 'settings'])>Settings</a>
-                <div class="submenu">
-                    <a href="{{ url('/admin/settings/add-admin') }}">Add New Admin</a>
-                    <a href="{{ url('/admin/settings/change-admin-password') }}">Change Admin Password</a>
-                    <a href="{{ url('/admin/settings/edit-admin') }}">Edit Admin Details</a>
-                    <a href="{{ url('/admin/settings/toggle-admin') }}">Toggle</a>
-                </div>
-            </div>
+            <a href="{{ route('admin.settings.index') }}" @class(['active' => ($activeNav ?? '') === 'settings'])>Settings</a>
         </nav>
     </aside>
 
